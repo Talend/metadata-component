29362c1f969e3407e5574bb76ee36867f2b5510a
Thibaut Gourdel <tgourdel@talend.com>
2020-06-26 10:24:59 +0200

    Change icon

--- src/main/java/com/talend/components/package-info.java
+++ src/main/java/com/talend/components/package-info.java
@@ -1,7 +1,7 @@
 // this tells the framework in which family (group of components) and categories (UI grouping)
 // the components in the nested packages belong to
 @Components(family = "metadata", categories = "Misc")
-@Icon(value = CUSTOM, custom = "metadata") // icon is located at src/main/resources/icons/metadata.svg
+@Icon(value = CUSTOM, custom = "shield-full-check") // icon is located at src/main/resources/icons/metadata.svg
 package com.talend.components;
 
 import static org.talend.sdk.component.api.component.Icon.IconType.CUSTOM;
